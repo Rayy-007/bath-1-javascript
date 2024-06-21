9c4093924d9996b1d3b2be3090f2dccc7bd73131
Rayy-007 <minkhant88887@gmail.com>
2024-06-21 20:27:37 +0630

    added loop file

---
+++ 07-loops.js
@@ -0,0 +1,43 @@
+const fruits = [
+  "Apple",
+  "Mango",
+  "Orange",
+  "Papaya",
+  "Pineapple",
+  "Strawberries",
+  "Olive",
+];
+
+/** ----------- For Loop ------------- */
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// for (initialization; condition; increment) {
+// }
+
+/** ----------- While Loop ------------- */
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+/** -----------  Do - While Loop ------------- */
+// let num = 0;
+// do {
+//   console.log(num);
+//   num++;
+// } while (num < 10);
+
+/** ----------- For Of  Loop ------------- */
+// for (let fruit of fruits) {
+//   console.log(fruit);
+// }
+
+/** ----------- For In  Loop ------------- */
+// const numbers = { num1: 1, num2: 2, num3: 3, num4: 4 };
+
+// for (let key in numbers) {
+//   console.log(numbers[key]);
+// }
